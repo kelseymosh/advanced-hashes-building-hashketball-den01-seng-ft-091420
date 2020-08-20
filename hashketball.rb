dab269d6a262dbaae01372fa7176a8945575068c
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-20 20:34:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,21 @@ def game_hash(game_data)
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      
+      :players => {
+        :player1 => {
+          :name => "Allen Iverson",
+          :number => 0,
+          :shoe => 16,
+          :stats => {
+            :points => 22,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 1    
+          }
+        }
+      }    
     }
   }
 end
