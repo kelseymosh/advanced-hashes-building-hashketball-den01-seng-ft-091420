207b7ea573cf5755f8336bfb30551fa06aa45f6e
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-20 21:21:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,148 +5,24 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
-        :player1 => {
-          :name => "Allen Iverson",
-          :number => 0,
-          :shoe => 16,
-          :stats => {
-            :points => 22,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 3,
-            :blocks => 1,
-            :slam_dunks => 1,
-          },
-        :player_2 => {
-          :name => "Reggie Evans",
-          :number => 30,
-          :shoe => 14,
-          :stats => {
-            :points => 12,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 12,
-            :blocks => 12,
-            :slam_dunks => 7,
-        },
-        :player_3 => {
-          :name => "Brook Lopez",
-          :number => 11,
-          :shoe => 17,
-          :stats => {
-            :points => 17,
-            :rebounds => 19,
-            :assists => 10,
-            :steals => 3,
-            :blocks => 1,
-            :slam_dunks => 15,
-        },
-        :player_4 => {
-          :name => "Mason Plumlee",
-          :number => 1,
-          :shoe => 19,
-          :stats => {
-            :points => 26,
-            :rebounds => 11,
-            :assists => 6,
-            :steals => 3,
-            :blocks => 8,
-            :slam_dunks => 5,
-        },
-        :player_5 => {
-          :name => "Jason Terry",
-          :number => 31,
-          :shoe => 15,
-          :stats => {
-            :points => 19,
-            :rebounds => 2,
-            :assists => 2,
-            :steals => 4,
-            :blocks => 11,
-            :slam_dunks => 1,
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    :away => {
-      :team_name => "Charlotte Hornets",
-      :colors => ["Turquoise", "Purple"],
-      :players => {
-        :player1 => {
-          :name => "Jeff Aldrien",
-          :number => 4,
-          :shoe => 18,
-          :stats => {
-            :points => 10,
-            :rebounds => 1,
-            :assists => 1,
-            :steals => 2,
-            :blocks => 7,
-            :slam_dunks => 2,
-          },
-        :player_2 => {
-          :name => "Bismack Biyombo",
-          :number => 0,
-          :shoe => 16,
-          :stats => {
-            :points => 12,
-            :rebounds => 4,
-            :assists => 7,
-            :steals => 22,
-            :blocks => 15,
-            :slam_dunks => 10,
-        },
-        :player_3 => {
-          :name => "Disagna Diop",
-          :number => 2,
-          :shoe => 14,
-          :stats => {
-            :points => 24,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 4,
-            :blocks => 5,
-            :slam_dunks => 5,
-        },
-        :player_4 => {
-          :name => "Ben Gordon",
-          :number => 8,
-          :shoe => 15,
-          :stats => {
-            :points => 33,
-            :rebounds => 3,
-            :assists => 2,
-            :steals => 1,
-            :blocks => 1,
-            :slam_dunks => 0,
-        },
-        :player_5 => {
-          :name => "Kemba Walker",
-          :number => 33,
-          :shoe => 15,
-          :stats => {
-            :points => 6,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 7,
-            :blocks => 5,
-            :slam_dunks => 12,
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+      :players => [
+        {},
+        {},
+        {},
+        {},
+        {},
+      ]
+    },
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => [
+      {},
+      {},
+      {},
+      {},
+      {},
+    ]
   }
 }
-  end
 end
