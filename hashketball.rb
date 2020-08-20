7279f68df649ed9ce483d1a4353121c392fb64ea
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-20 20:58:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,6 +136,10 @@ def game_hash
               :steals => 7,
               :blocks => 5,
               :slam_dunks => 12,
+                  }
+                }
+              }
+            }
           }
         }
       }
