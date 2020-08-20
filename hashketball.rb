040330e2efc9f8ad78ef4222a5261857a390b7f0
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-20 21:09:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -73,5 +73,78 @@ def game_hash
         }
       }
     }
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        :player1 => {
+          :name => "Allen Iverson",
+          :number => 0,
+          :shoe => 16,
+          :stats => {
+            :points => 22,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 1,
+          },
+        :player_2 => {
+          :name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :stats => {
+            :points => 12,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 12,
+            :blocks => 12,
+            :slam_dunks => 7,
+        },
+        :player_3 => {
+          :name => "Brook Lopez",
+          :number => 11,
+          :shoe => 17,
+          :stats => {
+            :points => 17,
+            :rebounds => 19,
+            :assists => 10,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 15,
+        },
+        :player_4 => {
+          :name => "Mason Plumlee",
+          :number => 1,
+          :shoe => 19,
+          :stats => {
+            :points => 26,
+            :rebounds => 11,
+            :assists => 6,
+            :steals => 3,
+            :blocks => 8,
+            :slam_dunks => 5,
+        },
+        :player_5 => {
+          :name => "Jason Terry",
+          :number => 31,
+          :shoe => 15,
+          :stats => {
+            :points => 19,
+            :rebounds => 2,
+            :assists => 2,
+            :steals => 4,
+            :blocks => 11,
+            :slam_dunks => 1,
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 end
